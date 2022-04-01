62173f8fc5c287f9c0329047e98e825c2dcdb2e3
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-01 08:32:00 -0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 09:02:08 2022
+
+@author: nvrmr
+"""
+
+print("Hello, world!")
+
+
+nome = input("What is your name? ")
+
+print(f"Hello, {nome}")
