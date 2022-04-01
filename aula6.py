690478569adbd5c8878065d2bc9f72b4aed39301
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-01 09:39:14 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 08:07:18 2022
+
+@author: nvrmr
+"""
+
+name = "Lenore"
+
+if name == "Nevermore":
+    print("\033[1;35;47mQuoth the Raven Nevermore\033[0m")
+
+if name != "Nevermore":
+    print("\033[1;37;41mBe that word our sign of parting, bird or fiend! Get thee back into the tempest and the Night's Plutonian shore! Leave no black plume as a token of that lie thy soul hath spoken! Leave my loneliness unbroken!- quit the bust above my door! Take thy beak from out my heart, and take thy form from off my door!\033[0m")
+
