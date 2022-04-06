8713b983c9f7a9e0eb44cfd5ca372845958001fc
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-06 16:01:50 -0300

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 15:19:14 2022
+
+@author: nvrmr
+"""
+
+name = "Nevermore"
+age = 22
+age_days = age * 365.25
+likes_rice = True
+course_fini = False
+
+time_until_vacation = "Too long"
+time_until_vacation = 120
+
+pet_list = ["Theo", "Vida", "Luna", "Diego", "Saori"]
+
+print(pet_list)
+print(pet_list[0])
