39bbc18ffdfa49ab67745c996d0163b7cd365e53
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-08 12:41:53 -0300

    Add files via upload

---
+++ function.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 11:12:06 2022
+
+@author: Nevermore
+"""
+
+def sum_numbers(a,b):
+    """ Sums two values
+    ----
+    Arguments:
+        a: first value
+        b: second value
+    """
+    return a + b
+
+print(sum_numbers(11,13))
---
+++ import.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 11:20:50 2022
+
+@author: Nevermore
+"""
+
+# this will not work unless the file 'function.py' is on the same folder
+
+import function
+print(function.sum_numbers(55,66))
