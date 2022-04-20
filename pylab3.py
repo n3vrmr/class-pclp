1af2bb8a3ae0577f373c4b2165be0a7ae508c396
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-20 15:36:50 -0300

    Add files via upload

---
+++ pylab3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 13:14:50 2022
+
+@author: nvrmr
+"""
+
+prof = {"name":"Álvaro", "age":33, "temperature":36.5, "has_cats":True}
+
+
+if prof["temperature"] < 38: #this line returns an error, console reads: "AttributeError: 'str' object has no attribute 'value'"
+    print("Álvaro peut entrer.")
+if prof["has_cats"] == True:
+    print("\033[1;34;47mProfesseur Álvaro a trois chats.\033[0m")
+    
+print(prof["age"])
+print(__name__)
+
+# sorting dictionaries inside a list
+# for <variable> in <list>:
+    # <list>.append(<variable>["<key>"])
+# print(sorted(<list>))
