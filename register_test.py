db7adeb5a9debabbaae5174589c60888d8304289
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-04-20 22:17:16 -0300

    Add files via upload

---
+++ register_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 17:03:25 2022
+
+@author: Nevermore
+"""
+
+users = []
+
+go = True
+while go:
+    go = False
+    user = {"name": " ", "age": int(), "registry": int(), "major": " "}
+    user['name'] = input("Name: ")
+    user["age"] = int(input("Age: "))
+    user["registry"] = int(input("Registry number: "))
+    user["major"] = input("Major: ")
+    users.append(user)
+    cont = input("\033[1;31;47mContinue? Reply Y for Yes or N for No.\033[0m ")
+    if cont == "Y":
+       go = True
+    elif cont == "N":
+       go = False
+
+# print(len(users))
